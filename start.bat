d64f60e14cc24196b5120abc5f6fb778f29f7694
CometWei <reveno415066@gmail.com>
2022-09-02 19:44:35 +0800

    修改：     start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,1 @@
-java --illegal-access=permit -noverify -javaagent:".\burp-loader-x-Ai.jar" -Xmx2048m -jar ".\burpsuite_pro_v2021.8.2.jar"
-pause
+java -noverify -javaagent:".\burp-loader-x-Ai.jar" -Xmx2048m -jar ".\burpsuite_pro_v2021.8.2.jar"
